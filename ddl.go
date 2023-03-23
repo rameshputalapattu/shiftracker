be1b5d7f513781f3cced113eb9fbd1ceec5ab0c4
ramesh putalapattu <rameshputalapattu@gmail.com>
2023-03-23 13:28:50 +0530

    updated table definition

--- ddl.go
+++ ddl.go
@@ -6,13 +6,16 @@ func createDDL(db *sqlx.DB) {
 
 	// Create the shifts table if it doesn't exist
 	db.MustExec(`CREATE TABLE IF NOT EXISTS shifts (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT,
-        shift_date TEXT,
-        shift_type TEXT,
-        task TEXT,
-        hours INTEGER,
-		created_timestamp TIMESTAMP default CURRENT_TIMESTAMP
-    )`)
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    shift_date TEXT,
+    shift_type TEXT,
+    task TEXT,
+    task_type TEXT,
+
+    hours INTEGER default 0,
+    minutes INTEGER default 0,
+    created_timestamp TIMESTAMP default CURRENT_TIMESTAMP
+  )`)
 
 }
